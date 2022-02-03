1a9083bbcde80b824924d10adcd200b7ecdfed0f
ValerianExp <74860846+ValerianExp@users.noreply.github.com>
2022-02-03 15:23:23 +0100

    Add files via upload

---
+++ TrafficSimulatorTP/src/simulator/model/Junction.java
@@ -0,0 +1,29 @@
+package simulator.model;
+
+import org.json.JSONObject;
+
+public class Junction extends SimulatedObject{
+
+	Junction(String id) {
+		super(id);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	void advance(int time) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public JSONObject report() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void enter(Vehicle vehicle) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
