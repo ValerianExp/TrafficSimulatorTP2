292bd8a846c55ef0a8eaf08a9e5cb17bd29ffbf9
jesuggc <jesugo11@ucm.es>
2022-02-05 22:12:29 +0100

    InterCityRoad bien subido jajaja

---
+++ src/simulator/model/InterCityRoad.java
@@ -0,0 +1,37 @@
+package simulator.model;
+
+public class InterCityRoad extends Road {
+
+	InterCityRoad(String id, Junction srcJunc, Junction destJunc, int maxSpeed, int contLimit, int length, Weather weather) {
+		super(id, srcJunc, destJunc, maxSpeed, contLimit, length, weather);
+		
+	}
+
+	@Override
+	public void reduceTotalContamination() {
+		totalCO2 = ((100 - getWeather().getValue()) * totalCO2)/100;
+		
+	}
+
+	@Override
+	public void updateSpeedLimit() {
+		if(totalCO2 > contLimit) {
+			speedLimit = maxSpeed/2;
+		}
+		else {
+			speedLimit = maxSpeed;
+		}
+		
+	}
+
+	@Override
+	public int calculateVehicleSpeed(Vehicle v) {
+		if (weather == Weather.STORM) {
+			return((speedLimit*8)/10);
+		}
+		else {
+			return(speedLimit);
+		}
+	}
+
+}
