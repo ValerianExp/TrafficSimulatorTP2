0bdcf2044e91fa9fcab8701f3a9dac3ecac349cc
RedValerian <r.pino.camacho@gmail.com>
2022-02-07 10:43:03 +0100

    Update

---
+++ src/simulator/model/RoundRobinStrategy.java
@@ -0,0 +1,14 @@
+package simulator.model;
+
+import java.util.List;
+
+public class RoundRobinStrategy implements LightSwitchingStrategy {
+
+    @Override
+    public int chooseNextGreen(List<Road> roads, List<List<Vehicle>> qa, int currGreen, int lastSwitchingTime,
+            int currTime) {
+        // TODO completar
+        return 0;
+    }
+
+}
