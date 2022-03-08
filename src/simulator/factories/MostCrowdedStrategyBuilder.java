ec241b9f1fd8e1934a818aed0dc6115d5cba7992
jesuggc <73794079+jesuggc@users.noreply.github.com>
2022-03-08 20:55:28 +0100

    Add files via upload
    MostCrowded y RoundRobin arreglado

--- src/simulator/factories/MostCrowdedStrategyBuilder.java
+++ src/simulator/factories/MostCrowdedStrategyBuilder.java
@@ -4,22 +4,19 @@ import org.json.JSONObject;
 
 import simulator.model.LightSwitchingStrategy;
 import simulator.model.MostCrowdedStrategy;
+import simulator.model.RoundRobinStrategy;
 
 public class MostCrowdedStrategyBuilder extends Builder<LightSwitchingStrategy>{
 
-	private MostCrowdedStrategy m;
+	private MostCrowdedStrategy m = new MostCrowdedStrategy(1);
 	public MostCrowdedStrategyBuilder() {
 		super("most_crowded_lss");
 	}
 
 	@Override
 	protected LightSwitchingStrategy createTheInstance(JSONObject data) {
-		if(data != null) {
-			if(data.has("timeslot")) {
-				m = new MostCrowdedStrategy(data.getInt("timeslot"));
-			}else {
-				m = new MostCrowdedStrategy((int)1);
-			}
+		if(data != null && data.has("timeslot")) {
+			m = new MostCrowdedStrategy(data.getInt("timeslot"));
 		}
 		
 		return m;
