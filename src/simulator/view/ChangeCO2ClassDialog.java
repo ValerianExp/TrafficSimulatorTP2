5b4e2c571f62763ca130e9ccc6c2ba27c7a85995
RedValerian <r.pino.camacho@gmail.com>
2022-04-04 10:46:10 +0200

    Update ChangeCO2ClassDialog.java

--- src/simulator/view/ChangeCO2ClassDialog.java
+++ src/simulator/view/ChangeCO2ClassDialog.java
@@ -1,6 +1,15 @@
 package simulator.view;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+
+import javax.swing.JComboBox;
 import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import simulator.model.Vehicle;
 
 public class ChangeCO2ClassDialog extends JDialog{
 	
@@ -10,8 +19,33 @@ public class ChangeCO2ClassDialog extends JDialog{
 	}
 
 	private void initGUI() {
-		JDialog dialog = new JDialog();
+		//TODO Esta mal, pero funciona
+		//Creo que hay que poner toptext, selectores, y botones
+		//En su propio JPanel de cada uno
+		this.setLayout(new BorderLayout());
+		//top text(?)
+		JLabel toptext = new JLabel();
+		toptext.setText("Schedule an event to change the CO2 Class of a vehicle after a given number of simultation ticks from now");
+		this.add(toptext, BorderLayout.NORTH);
 		
+		//selectores
+		JPanel selectors = new JPanel();
+		this.add(selectors, BorderLayout.CENTER);
+		JLabel v = new JLabel();
+		v.setText("Vehicle: ");
+		//TODO Necesita la informacion de los vehicles
+		JComboBox<Vehicle> vehicleCB = new JComboBox<Vehicle>();
+		vehicleCB.setPreferredSize(new Dimension(100, 20));
+		
+		JComboBox<Integer> co2Class = new JComboBox<Integer>();
+		co2Class.setPreferredSize(new Dimension(100, 20));
+		JLabel c = new JLabel();
+		c.setText("CO2 Class: ");
+		selectors.add(v);
+		selectors.add(vehicleCB);
+		selectors.add(c);
+		selectors.add(co2Class);
+		this.pack();
 	}
 	
 }
